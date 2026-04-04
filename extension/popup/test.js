163a8b7dcd420996539417b58ff26d6da05dfc66
ttai2023 <territai0831@gmail.com>
2026-04-04 11:53:01 -0700

    testing log

--- extension/popup/test.js
+++ extension/popup/test.js
@@ -1,3 +1,6 @@
 // Test script for popup functionality
-
-console.log("Popup script loaded successfully!");
+// run when click me pressed from popup.html
+document.getElementById("clickMe").addEventListener("click", function() {
+    alert("Button clicked!");
+    console.log("Button was clicked in the popup!");
+});
